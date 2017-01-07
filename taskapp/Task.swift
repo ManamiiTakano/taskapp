7189a90384e78eb7d56dd149619be7c080e5ed1f
ManamiiTakano <manami.mark07@gmail.com>
2017-01-07 21:21:47 +0900

    タスクアプリ

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 高野 眞奈美 on 2016/12/25.
+//  Copyright © 2016年 manami.takano. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    //カテゴリー
+    dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
